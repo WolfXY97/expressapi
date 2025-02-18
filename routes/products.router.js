8229991fef152db7dc4e07b0adcc211b5ad52049
Mateo Riofrio <joriofrio1997@gmail.com>
2025-02-17 22:37:49 -0500

    Metodos HTTP

--- routes/products.router.js
+++ routes/products.router.js
@@ -26,11 +26,40 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
+  if (id === "999") {
+    res.status(404).json({ message: "Not found"})
+  }
+  res.status(200).json({
     id,
     name: 'Laptop',
     price: 1500
   })
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  })
+} );
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id
+  });
 });
 
 module.exports = router;
